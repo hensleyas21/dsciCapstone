aee63e1b2b46f674f5334205b34578925be7acea
hensleyas21 <hensleyas21@gcc.edu>
2025-02-13 19:36:35 -0500

    make_barplot

--- data_cleaning/austin_delete_this.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-from data_inspector import make_plot, make_density_plot, make_boxplot
-
-if __name__ == '__main__':
-    data = [1, 2, 3, 4, 1, 2, 3, 3, 3, 4, 5, 2, 2, 2]
-    make_boxplot(data).show()
