1384b91d5052f9a686c906d40137e1829dc47bb9
Stevie Michalik <michaliksj21@gcc.edu>
2025-02-15 16:23:30 -0500

    Wrote code to load and save a pandas df. Added a TODO statement for the load_data function as there is still some confusion about what exactly to do with the missing argument.

--- data_cleaning/data_loader.py
+++ data_cleaning/data_loader.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from numpy.typing import DTypeLike
+import glob
+import os
 
 def load_data(path: str, columns: dict[str,DTypeLike], missing: dict[str,set[str]]) -> pd.DataFrame:
     """Loads the raw dataset from files using parameters from the config file.
-    
+
     Positional Arguments:
     path    - The path to the directory or file where your data is located
     columns - The columns from the dataset you plan to use mapped to their data types
@@ -11,8 +13,30 @@ def load_data(path: str, columns: dict[str,DTypeLike], missing: dict[str,set[str
 
     returns a DataFrame loaded from the filepath given with the specified columns and types
     """
-    raise NotImplementedError('TODO: Implement this function')
+    csv_files = glob.glob(os.path.join(path, '/*.csv'))
+
+    dataframes = []
+    for filename in csv_files:
+        df = pd.read_csv(filename, index_col=None, header=0)
+        dataframes.append(df)
+
+    df = pd.concat(dataframes, axis=0, ignore_index=True)
+
+    # keep only the columns we want to keep.
+    df = df[columns.keys()]
+
+    # for each column we keep, convert the column to numeric or categorical based upon the variable type
+    for key, value in columns.items():
+        if value == 'real':
+            df[key] = pd.to_numeric(df[key], errors='coerce')
+        elif value == 'nominal':
+            df[key] = df[key].astype('category')
+
+    # @TODO: confused about what to do with the missing values
+
+    return df
+
 
 def save_data(df: pd.DataFrame, path: str) -> None:
     """Saves the transformed dataset into the specified output file"""
-    raise NotImplementedError('TODO: Implement this function')
+    df.to_csv(path)
