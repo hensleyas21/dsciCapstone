81e891218e600be6c07a1f7f960a9c804c4cc54c
Stevie Michalik <michaliksj21@gcc.edu>
2025-04-02 10:57:37 -0400

    Stacked bar chart file

---
+++ visualizations/stacked_bar_chart.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+SEASON = 2024
+
+def output_stacked_bar_chart():
+    df = pd.read_csv('../cleaned_data.csv')
+
+    # Stacked Bar Chart: Show each team on the X-axis, Play type (just run and pass) on the Y-axis. Just for certain seasons
+
+    # Exclude all values that do not match the current season
+    df = df[df['season'] == SEASON]
+
+    # Exclude all values that aren't run or pass in play_type
+    df['play_type'] = df['play_type'][df['play_type'].isin(['run', 'pass'])]
+    df = df[['play_type', 'posteam']]
+
+    # Group by team, get count of pass and run
+    df = df.groupby(['posteam']).agg(
+        run_count=('play_type', lambda x: (x == 'run').sum()),
+        pass_count=('play_type', lambda x: (x == 'pass').sum()),
+        total=('play_type', lambda x: (x.isin(['run', 'pass'])).sum()),
+    ).reset_index()
+
+    df['run_ratio'] = df['run_count'] / df['total']
+    df['pass_ratio'] = df['pass_count'] / df['total']
+
+    df = df[['posteam', 'run_ratio', 'pass_ratio']]
+
+    df = df.sort_values(by='run_ratio', ascending=False)
+
+    df = df.set_index('posteam')
+    df.plot(kind='bar', stacked=True)
+    plt.savefig('stacked_bar_chart.png')
+
+
+if __name__ == '__main__':
+    output_stacked_bar_chart()
