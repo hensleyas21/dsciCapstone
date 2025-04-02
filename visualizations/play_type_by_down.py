b98bb972cae8e49cefc817db6043836a4d3495f8
hensleyas21 <hensleyas21@gcc.edu>
2025-04-02 10:47:29 -0400

    Create play_type_by_down.py

---
+++ visualizations/play_type_by_down.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('cleaned_data.csv')
+
+plot = sns.histplot(data=df, x='down', hue='play_type', multiple='stack', discrete=True)
+plot.set_title("Play Type by Down")
+plt.xlabel("Down")
+plt.xticks([1, 2, 3, 4])
+legend = plot.legend_
+plt.legend(handles=legend.legend_handles, labels=[t.get_text().replace('_', ' ').title() for t in legend.texts], 
+            title="Play Type", loc='upper right')
+plt.ylabel('Number of Plays')
+plt.show()
