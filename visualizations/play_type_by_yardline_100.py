547f9a6eb1e22cc3658c56ba2e98da47539f1061
hensleyas21 <hensleyas21@gcc.edu>
2025-04-04 10:43:09 -0400

    Graphs

--- visualizations/play_type_by_yardline_100.py
+++ visualizations/play_type_by_yardline_100.py
@@ -4,10 +4,10 @@ import seaborn as sns
 
 df = pd.read_csv('cleaned_data.csv')
 
-plot = sns.histplot(data=df, x='down', hue='play_type', multiple='stack', discrete=True)
-plot.set_title("Play Type by Down")
-plt.xlabel("Down")
-plt.xticks([1, 2, 3, 4])
+plot = sns.histplot(data=df, x='yardline_100', hue='play_type', multiple='stack', discrete=True)
+plot.set_title("Play Type by Yardline 100")
+plt.xlabel("Yardline 100")
+plt.xlim(0, 100)
 legend = plot.legend_
 plt.legend(handles=legend.legend_handles, labels=[t.get_text().replace('_', ' ').title().replace('Qb', 'QB') for t in legend.texts], 
             title="Play Type", loc='upper right')
